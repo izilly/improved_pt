7b30938b22e7c6a10a2691ad569746e5661d00cd
mr_wrong <mr_wrong@pt>
2016-04-13 04:46:00 -0400

    Linting, Refactoring

--- background.js
+++ background.js
@@ -1,24 +1,30 @@
 chrome.extension.onRequest.addListener(
   function(request, sender, sendResponse) {
 	if (request.set === "show")
-	  sendResponse({setShow: localStorage["show"], setColor: localStorage["color"], setVideo: localStorage["video"], setQuotes: localStorage["quotes"], setReload: localStorage["reload"], setScroll: localStorage["scroll"]});
+	  sendResponse({setShow: localStorage["show"], setColor: localStorage["color"], setVideo: localStorage["video"], setQuotes: localStorage["quotes"], setReload: localStorage["reload"], setScroll: localStorage["scroll"], setSfw: localStorage["sfw"]});
 	else if (request.set === "index")
-		sendResponse({setFirst: localStorage["first"]});
+		sendResponse({setFirst: localStorage["first"], setSfw: localStorage["sfw"]});
 	else
 	  sendResponse({}); // snub them.
   });
 if (localStorage["show"] !== "load") {
-	var contextMenu = chrome.contextMenus.create({"title": "Open All Links", 
-									 "contexts": ["page","selection","link","editable","audio","video"],
-									 "onclick": send});
+	var contextMenu = chrome.contextMenus.create({
+		"title": "Open All Links",
+		"contexts": ["page","selection","link","editable","audio","video"],
+		"onclick": send
+	});
 }
-var closeMenu = chrome.contextMenus.create({"title": "Close Image",
-								"contexts": ["image"],
-								"onclick": close}); 
+var closeMenu = chrome.contextMenus.create({
+	"title": "Close Image",
+	"contexts": ["image"],
+	"onclick": close
+}); 
 
-var contextMenu = chrome.contextMenus.create({"title": "Close All Links", 
-								 "contexts": ["page","selection","link","editable","audio","video"],
-								 "onclick": closeAll});
+var contextMenu = chrome.contextMenus.create({
+	"title": "Close All Links",
+	"contexts": ["page","selection","link","editable","audio","video"],
+	"onclick": closeAll
+});
 
 function send() {
 	chrome.tabs.getSelected(null, function(tab) {
