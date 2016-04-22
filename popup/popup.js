fc0b8099f31ae947f4a2172f3c43a36953eb6950
Will Adams <willadams+dev@gmail.com>
2016-04-22 12:52:04 -0400

    modify popup.js to work in firefox

--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
 	document.getElementById('optionsLink').style.cursor = "pointer";
 	document.getElementById('optionsLink').onclick = function (e) {
-		chrome.runtime.openOptionsPage(function () {
-		//console.log('options');
+		var optionsUrl = chrome.extension.getURL('options/options_ui.html');
+		chrome.tabs.query({'title': "PT'd Settings"}, function(tabs) {
+			if (tabs[0] && tabs[0].url == optionsUrl) {
+				chrome.tabs.update(tabs[0].id, {active: true});
+			} else {
+				chrome.tabs.create({url: optionsUrl, active: true});
+			}
 		});
 	};
 });
